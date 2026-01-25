b8139fcfc8b0f6fe8cd179ba53be8524a6194b89
Ravi Pangali <lilathapamagar453@gmail.com>
2026-01-25 18:32:44 +0545

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "My Cafe-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
